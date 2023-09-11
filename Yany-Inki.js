613d7c30d2ea76c87489f428e319c94173d1f92c
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-11 16:20:39 -0300

    Create Yany-Inki.js

---
+++ Yany-Inki.js
@@ -0,0 +1,15 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "yany_inki"
+var name = "Yany Inki"
+var description = "No Theory description.";
+var authors = "JojoGames320";
+var version = "1.0.0";
+
+var currency;
+
+currency = theory.createCurrency();
